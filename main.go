3d70fce319aa35f0b86424c7349e766b50833077
Moacyr Jr Trombetta <moacyr.trombetta@olist.com>
2021-10-06 01:02:03 -0300

    db connection

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	//command: go mod init
+	_ "github.com/lib/pq"
 )
 
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+func dbConnection() *sql.DB {
+	connection := "user=postgres dbname=postgres password=Teste123* host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 type Product struct {
+	Id          int
 	Name        string
 	Description string
 	Price       float64
 	Amount      int
 }
-
-var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", index)
@@ -20,11 +35,36 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	products := []Product{
-		{Name: "Mouse", Description: "Microsoft 1.1", Price: 95.00, Amount: 12},
-		{"Keyboard", "Logitech", 150.00, 5},
-		{"Camera", "Logitech", 250.00, 7},
+	db := dbConnection()
+	findAllProducts, err := db.Query("select * from product")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Product{}
+	products := []Product{}
+
+	for findAllProducts.Next() {
+		var id, amount int
+		var name, description string
+		var price float64
+
+		err = findAllProducts.Scan(&id, &name, &description, &price, &amount)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id = id
+		p.Name = name
+		p.Description = description
+		p.Price = price
+		p.Amount = amount
+
+		products = append(products, p)
 	}
 
 	templates.ExecuteTemplate(w, "Index", products)
+	db.Close()
 }
