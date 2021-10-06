6c91258bb89ee161df19d63b246227d8c98991ee
Moacyr Jr Trombetta <moacyr.trombetta@olist.com>
2021-10-05 23:29:53 -0300

    creation html with bootstrap

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Amount      int
+}
+
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{Name: "Mouse", Description: "Microsoft 1.1", Price: 95.00, Amount: 12},
+		{"Keyboard", "Logitech", 150.00, 5},
+		{"Camera", "Logitech", 250.00, 7},
+	}
+
+	templates.ExecuteTemplate(w, "Index", products)
 }
